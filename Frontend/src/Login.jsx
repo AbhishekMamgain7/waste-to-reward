9811fdb84fbf98824069ca63b5719f561de05c78
Abhishek Mamgain <93778720+AbhishekMamgain7@users.noreply.github.com>
2025-03-13 20:56:48 +0000

    Added Signup Page

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -16,6 +16,7 @@ const Login = () => {
 
   return (
     <div className="login-container">
+      <div className="login-overlay"></div>
       <motion.div
         initial={{ opacity: 0, y: -50 }}
         animate={{ opacity: 1, y: 0 }}
