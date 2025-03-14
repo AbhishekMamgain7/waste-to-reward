146da5c691f9b8eed2009c94f68c5eb9833817f6
AbhishekMamgain7 <93778720+AbhishekMamgain7@users.noreply.github.com>
2025-03-15 01:12:30 +0530

    Added Login Cred

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -1,19 +1,53 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
-// import { app } from "./firebaseConfig";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "./firebaseConfig";
+
 import "./Login.css";
 
 const Login = () => {
+  const auth = getAuth(app);
+  const navigate = useNavigate();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleLogin = (e) => {
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleLogin = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
-    setTimeout(() => {
-      alert("Login Successful!");
+
+    const { email, password } = formData;
+
+    if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      alert("Invalid email format.");
+      setFormData({ email: "", password: "" });
       setIsSubmitting(false);
-    }, 2000);
+      return;
+    }
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert("Logged in successfully!");
+      setFormData({ email: "", password: "" });
+      navigate("/dashboard");
+    } catch (error) {
+      if (error.code === "auth/invalid-credential") {
+        alert("Incorrect username/password. Please try again.");
+        setFormData({ email: "", password: "" });
+      } else {
+        alert("Error: " + error.message);
+      }
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   return (
@@ -30,16 +64,22 @@ const Login = () => {
 
         <form onSubmit={handleLogin}>
           <motion.input
+            name="email"
             whileFocus={{ scale: 1.05 }}
             type="email"
             placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
             required
             className="login-input"
           />
           <motion.input
+            name="password"
             whileFocus={{ scale: 1.05 }}
             type="password"
             placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
             required
             className="login-input"
           />
